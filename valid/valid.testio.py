f708e89cfdd98c19a0e3ae35f9643f038fbe6cdc
mustafatekin1 <88633263+mustafatekin1@users.noreply.github.com>
2022-12-24 21:22:27 +0300

    Update from github

--- valid/valid.testio.py
+++ valid/valid.testio.py
@@ -1,6 +1,6 @@
 list1 = [1, 3, 3, 5, 7, 7, 1, 1, 1, 4, 4, 4, 12, 12, 12, 12]
 set1 = set(list1)
 print(set1)
-print("set len = ", len(set1))
+print("Set has just unique elements; set len = ", len(set1))
 
-print("list len = ", len(list1))
+print("List has all including unique and repetitive elements; list len = ", len(list1))
